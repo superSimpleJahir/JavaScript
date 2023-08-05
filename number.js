fedadea6313d464970f8caf061046df022c7e43b
superSimpleJahir <supersimple.jahir@gmail.com>
2023-08-05 14:30:56 +0600

    first commit

---
+++ number.js
@@ -0,0 +1,32 @@
+// Infinity Number 
+let x = 25 / 0;
+console.log(x);
+
+let y = Infinity;
+console.log(y);
+
+// HexaDecimal Number
+let a = 0xbb;
+console.log(a);
+
+// Number to String convart
+let b = a.toString();
+console.log(b);
+
+// Floating number string convart
+let c = 100;
+console.log(typeof (c));
+
+let d = c.toString();
+console.log(typeof (d));
+
+
+// Number baineary octa hexa convart
+let e = c.toString(2);
+console.log(e);
+e = c.toString(8);
+console.log(e);
+e = c.toString(16);
+console.log(e);
+
+let f = c.toPrecision(2);
